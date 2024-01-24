92aafa3600a803ba88bfcd55f76a10f4a53b1a81
Zachary Wagner <zwag14@yahoo.com>
2024-01-24 00:41:29 -0600

    ux enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,16 +74,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
-    console.log(ui);
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function(event, ui) {
-    console.log(ui);
+    $(this).removeClass('dropover');
+    $('.bottom-trrash').removeClass('dropover-active');
   },
   over: function(event) {
-    console.log(event);
+    $(event.target).addClass('dropover-active');
   },
   out: function(event) {
-    console.log(event);
+    $(event.target).removeClass('dropover-active');
   },
   update: function() {
     var tempArr = [];
@@ -129,10 +131,10 @@ $("#trash").droppable({
 
   },
   over: function(event, ui) {
-    console.log(ui);
+    $('.bottom-trash').addClass('bottom-trash-active');
   },
   out: function(event, ui) {
-    console.log(ui);
+    $('.bottom-trash').removeClass('bottom-trash-active');
   }
 });
 
@@ -153,7 +155,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
